79906b49f0299e1596c2c0930d2ca4717ce47ca1
João Pedro R <162389145+Joao-Pedro-R@users.noreply.github.com>
2025-05-08 21:37:44 -0300

    Add files via upload

---
+++ CP2-JAVA/Fab_brinquedo/src/main/java/com/empresa/brinquedos/BrinquedoApplication.java
@@ -0,0 +1,12 @@
+package com.empresa.brinquedos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrinquedoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrinquedoApplication.class, args);
+    }
+}
+
---
+++ CP2-JAVA/Fab_brinquedo/src/main/java/com/empresa/brinquedos/repository/BrinquedoRepository.java
@@ -0,0 +1,9 @@
+package com.empresa.brinquedos.repository;
+
+import com.empresa.brinquedos.model.Brinquedo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrinquedoRepository extends JpaRepository<Brinquedo, Long> {
+}
